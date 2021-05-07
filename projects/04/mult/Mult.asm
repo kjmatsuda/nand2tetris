56497569d0da18a669bb543d7238a34e188777ff
Koji Matsuda <kjmatsuda1985@gmail.com>
2021-05-07 10:25:25 +0900

    「4章　機械語」の Mult.asm の実装

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -8,5 +8,21 @@
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
-
-// Put your code here.
+    @2
+    M=0     // R2 を 0 で初期化
+(LOOP)
+    @1
+    D=M     // R1 を D に設定
+    @END
+    D;JLE
+    @1
+    M=D-1   // R1 をデクリメント
+    @0
+    D=M     // R0 を D に設定
+    @2
+    M=D+M   // R2 に R0 の値を加算
+    @LOOP
+    0;JMP   // ループ先頭に戻る
+(END)
+    @END
+    0;JMP
